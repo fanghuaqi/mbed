746456334ff3a9cf7ebe91dc81f2cfbe0f67ef0b
huaqi fang <Huaqi.Fang@synopsys.com>
2014-10-21 11:05:28 +0800

    Revert "no message"
    This reverts commit b72158c171bd968a9382e770f74a09a4403963c1.

---
+++ workspace_tools/host_tests/hello_auto.py
@@ -0,0 +1,49 @@
+"""
+mbed SDK
+Copyright (c) 2011-2013 ARM Limited
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+"""
+
+from host_test import DefaultTest
+from sys import stdout
+import re
+
+class HelloTest(DefaultTest):
+    HELLO_WORLD = "Hello World"
+
+    def run(self):
+        c = self.mbed.serial_readline()
+        if c is None:
+           self.print_result(self.RESULT_IO_SERIAL)
+           return
+        self.notify(c.strip())
+
+        c = self.mbed.serial_readline()
+        if c is None:
+           self.print_result(self.RESULT_IO_SERIAL)
+           return
+        self.notify("Read %d bytes"% len(c))
+        self.notify(c.strip())
+
+        result = True
+        # Because we can have targetID here let's try to decode
+        if len(c) < len(self.HELLO_WORLD):
+            result = False
+        else:
+            result = self.HELLO_WORLD in c
+        self.print_result(self.RESULT_SUCCESS if result else self.RESULT_FAILURE)
+
+
+if __name__ == '__main__':
+    HelloTest().run()
